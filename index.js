af8618063ba0706ca0c2044e61be0f30a34ab0f9
TowEx-MD <hubg97193@gmail.com>
2025-06-03 09:16:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-const express = require("express");
-const app = express();
-__path = process.cwd();
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let code = require("./pair");
-require("events").EventEmitter.defaultMaxListeners = 500;
-app.use("/code", code);
 
-app.use("/", async (req, res, next) => {
-  res.sendFile(__path + "/pair.html");
+import express from 'express';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Importing the 'pair' module
+import code from './pair.js';
+
+const app = express();
+
+// Resolve the current directory path in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 8000;
+
+import('events').then(events => {
+    events.EventEmitter.defaultMaxListeners = 500;
+});
+
+app.use('/code', code);
+app.use('/pair', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'pair.html'));
+});
+app.use('/', async (req, res) => {
+    res.sendFile(path.join(__dirname, 'main.html'));
 });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
-  console.log(`⏩ Server running on http://localhost:` + PORT);
+    console.log(`𝐏𝙾𝚆𝙴𝚁𝙳 𝐛𝐲 𝐐𝐔𝐄𝐄𝐍-𝐍𝐈𝐌𝐀𝐒𝐇𝐀-𝐌𝐃\n\nServer running on http://localhost:${PORT}`);
 });
 
-module.exports = app;
+export default app;
